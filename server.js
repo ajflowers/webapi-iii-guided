dc7109bfd6f82537574f2c7ac2a1ea9b448046f1
Angela Flowers <me@angela.flowers>
2019-10-30 13:30:56 -0400

    modified gatekeeper function

--- server.js
+++ server.js
@@ -18,11 +18,36 @@ function requestPeeper(req, res, next) {
   next();
 }
 
+
+// change the gatekeeper to return a 400 if no password is provided and a message
+// that says please provide a password
+// if a password is provided and it is mellon, call next, otherwise return a 401
+// and the you shall not pass message
+
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+
+  if (password === '') {
+    res.status(400).json({ error: 'please provide a password' })
+  } else if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(401).json({ you: 'shall not pass!!' });
+  }
+}
+
+
+
+
+
 // global middleware
 server.use(helmet()); //3rd party
 server.use(express.json()); //built-in
 server.use(dateLogger); //custom middleware 
 server.use(requestPeeper);
+server.use(gateKeeper);
 
 server.use('/api/hubs', hubsRouter);
 
